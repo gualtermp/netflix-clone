86eb6fb1ada8e1fc71b8235852cb3992ef576989
gualter <gualterparada@gmail.com>
2024-02-26 09:43:21 +0000

    add movie card

---
+++ app/components/MovieCard.tsx
@@ -0,0 +1,62 @@
+import { Button } from "@/components/ui/button";
+import { HeartIcon, PlayCircle } from "lucide-react";
+
+type MovieCard = {
+  title: string;
+  overview: string;
+  movieId: number;
+  isWatchList: boolean;
+  watchListId: string;
+  youtubeURL: string;
+  year: number;
+  age: number;
+  time: number;
+};
+
+export function MovieCard({
+  title,
+  overview,
+  movieId,
+  isWatchList,
+  watchListId,
+  youtubeURL,
+  year,
+  age,
+  time,
+}: MovieCard) {
+  return (
+    <>
+      <button className="-mt-14">
+        <PlayCircle className="h-20 w-20" />
+      </button>
+      <div className="right-5 top-5 absolute z-10">
+        {isWatchList ? (
+          <form>
+            <Button variant="outline" size="icon">
+              <HeartIcon className="w-4 h-4 text-red-500" />
+            </Button>
+          </form>
+        ) : (
+          <form>
+            <Button variant="outline" size="icon">
+              <HeartIcon className="w-4 h-4" />
+            </Button>
+          </form>
+        )}
+      </div>
+      <div className="p-5 absolute bottom-0 left-0">
+        <h1 className="font-bold text-lg line-clamp-1"> {title} </h1>
+        <div className="flex gap-x-2 items-center">
+          <p className="font-normal text-sm"> {year} </p>
+          <p className="font-normal border py-0.5 px-1 border-gray-200 rounded text-sm">
+            {age}+
+          </p>
+          <p className="font-normal text-sm"> {time}h</p>
+        </div>
+        <p className="line-clamp-1 text-sm text-gray-200 font-light">
+          {overview}
+        </p>
+      </div>
+    </>
+  );
+}
--- app/components/RecentlyAdded.tsx
+++ app/components/RecentlyAdded.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import prisma from "../utils/db";
+import { MovieCard } from "./MovieCard";
 
 async function getMostRecentMovies() {
   const movies = await prisma.movie.findMany({
@@ -10,6 +11,9 @@ async function getMostRecentMovies() {
       WatchLists: true,
       imageString: true,
       videoSource: true,
+      age: true,
+      release: true,
+      duration: true,
     },
     orderBy: {
       createdAt: "desc",
@@ -32,8 +36,31 @@ export default async function RecentlyAdded() {
             alt="movie"
             width={500}
             height={400}
-            className="rounded-sm absolute w-full h-full"
+            className="rounded-sm absolute w-full h-full object-cover"
           />
+          <div className="h-60 relative z-10 w-full transform transition duration-500 hover:scale-125 opacity-0 hover:opacity-100">
+            <div className="bg-gradient-to-bottom from-transparent via-black/50 to-black z-10 w-full h-full rounded-lg flex items-center justify-center border">
+              <Image
+                src={movie.imageString}
+                alt="movie"
+                width={500}
+                height={400}
+                className="rounded-lg absolute w-full h-full -z-10 object-cover"
+              />
+              <MovieCard
+                key={movie.id}
+                movieId={movie.id}
+                overview={movie.overview}
+                title={movie.title}
+                watchListId={movie.WatchLists[0]?.id}
+                youtubeURL={movie.videoSource}
+                isWatchList={movie.WatchLists.length > 0 ? true : false}
+                year={movie.release}
+                age={movie.age}
+                time={movie.duration}
+              />
+            </div>
+          </div>
         </div>
       ))}
     </div>
